aa3d66bcfe7c50497762154764d265c5c6992e61
0xKirkjufell <96358263+0xKirkjufell@users.noreply.github.com>
2022-01-09 01:57:19 -0500

    deploy vault settings

---
+++ deploy/008_vault_settings.js
@@ -0,0 +1,79 @@
+const { utils } = require("ethers");
+const hre = require("hardhat");
+
+const path = require("path");
+const { getTxOpts } = require("../utils/tx");
+const {
+  getAssetAddresses,
+  isFuji,
+  isFujiFork,
+  isTest,
+} = require("../test/helpers.js");
+const { log, withConfirmation } = require("../utils/deploy");
+
+/**
+ * Set default strategies for USDT, USDC, DAI
+ */
+const setVaultSettings = async () => {
+  const { guardianAddr } = await getNamedAccounts();
+  const governorAddr = guardianAddr;
+  // Signers
+  const sGovernor = await ethers.provider.getSigner(governorAddr);
+
+  const cVault = await ethers.getContractAt(
+    "VaultAdmin",
+    (
+      await ethers.getContract("VaultProxy")
+    ).address
+  );
+
+  // Set Vault buffer
+  await withConfirmation(
+    cVault
+      .connect(sGovernor)
+      .setVaultBuffer(utils.parseUnits("2", 16), await getTxOpts())
+  );
+  log("set vault buffer");
+
+  // Set Redeem fee BPS
+  await withConfirmation(
+    cVault.connect(sGovernor).setRedeemFeeBps(50),
+    await getTxOpts()
+  );
+  log("Set redeem free bps");
+
+  // FIXME: update with pangolin for WAVAX->USD?
+  // Set Uniswap addr
+  // await withConfirmation(
+  //   cVault
+  //     .connect(sGovernor)
+  //     .setUniswapAddr(
+  //       "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+  //       await getTxOpts()
+  //     )
+  // );
+  // log("Set Uniswap address");
+
+  // Set strategist addr
+  await withConfirmation(
+    // TODO update with strategist!
+    cVault.connect(sGovernor).setStrategistAddr(governorAddr, await getTxOpts())
+  );
+  log("Set strategist address");
+};
+
+const baseName = path.basename(__filename);
+const main = async () => {
+  console.log(`Running ${baseName} deployment...`);
+  await setVaultSettings();
+
+  console.log(`${baseName} deploy done.`);
+  return true;
+};
+
+main.id = baseName;
+main.dependencies = ["proxies"];
+main.tags = ["vaultSettings"];
+main.skip = () => isTest;
+
+module.exports = main;

1cce2eaa4fe7145fb2f6c10fe5a73e9536bfa7ee
0xKirkjufell <96358263+0xKirkjufell@users.noreply.github.com>
2022-01-09 23:21:46 -0500

    claim governance using proposal

--- deploy/009_newGovernor.js
+++ deploy/009_newGovernor.js
@@ -1,63 +1,64 @@
 const hre = require("hardhat");
 
 const path = require("path");
-const { log, withConfirmation } = require("../utils/deploy");
-const { getTxOpts } = require("../utils/tx");
+const { log, executeProposal, sendProposal } = require("../utils/deploy");
+const { isTest, isFork, isMainnet } = require("../test/helpers");
+const { proposeArgs } = require("../utils/governor");
 
 const claimGovernance = async () => {
-  const { deployerAddr, guardianAddr, governorAddr } =
-    await hre.getNamedAccounts();
-  const newGovernor = await ethers.getContractAt("Governor", governorAddr);
-  const sDeployer = await ethers.provider.getSigner(deployerAddr);
-  const sGuardian = await ethers.provider.getSigner(guardianAddr);
+  const { guardianAddr, governorAddr } = await hre.getNamedAccounts();
+
   const cXUSDProxy = await ethers.getContract("XUSDProxy");
   const cVaultProxy = await ethers.getContract("VaultProxy");
   const cAaveStrategyProxy = await ethers.getContract("AaveStrategyProxy");
-  const cXUSD = await ethers.getContractAt("XUSD", cXUSDProxy.address);
-  const cVault = await ethers.getContractAt("VaultAdmin", cVaultProxy.address);
-  const cAaveStrategy = await ethers.getContractAt(
-    "AaveStrategy",
-    cAaveStrategyProxy.address
-  );
-  await withConfirmation(
-    cVault.connect(sDeployer).transferGovernance(newGovernor.address)
-  );
-  log(`Transfer governance of vault with ${newGovernor.address}`);
-  await withConfirmation(
-    cVault.connect(sDeployer).claimGovernance(await getTxOpts())
-  );
-  log(`Claimed governance of vault with ${newGovernor.address}`);
+  const cFlipper = await ethers.getContract("Flipper");
 
-  await withConfirmation(
-    cXUSD.connect(sGuardian).transferGovernance(newGovernor.address)
+  const contracts = [cXUSDProxy, cVaultProxy, cAaveStrategyProxy, cFlipper];
+  const propTransferDescription = "Transfer governance to 1-minute governor";
+  const propTransferArgs = await proposeArgs(
+    contracts.map((contract) => {
+      return {
+        contract: contract,
+        signature: "transferGovernance(address)",
+        args: [governorAddr],
+      };
+    })
   );
-  await withConfirmation(cXUSD.connect(sGuardian).claimGovernance());
-  log(`Claimed governance of XUSD with ${newGovernor.address}`);
-  await withConfirmation(
-    cAaveStrategy.connect(sGuardian).transferGovernance(newGovernor.address)
-  );
-  await withConfirmation(cAaveStrategy.connect(sGuardian).claimGovernance());
-  log(`Claimed governance of AaveStrategy with ${newGovernor.address}`);
 
-  await withConfirmation(
-    cVaultProxy.connect(sDeployer).transferGovernance(newGovernor.address)
+  const propClaimDescription = "Transfer governance to 1-minute governor";
+  const propClaimArgs = await proposeArgs(
+    contracts.map((contract) => {
+      return {
+        contract: contract,
+        signature: "claimGovernance()",
+      };
+    })
   );
-  await withConfirmation(cVaultProxy.connect(sDeployer).claimGovernance());
-  log(`Claimed governance of vault proxy with ${newGovernor.address}`);
-  await withConfirmation(
-    cXUSDProxy.connect(sGuardian).transferGovernance(newGovernor.address)
-  );
-  await withConfirmation(cXUSDProxy.connect(sGuardian).claimGovernance());
-  log(`Claimed governance of XUSD proxy with ${newGovernor.address}`);
-  await withConfirmation(
-    cAaveStrategyProxy
-      .connect(sGuardian)
-      .transferGovernance(newGovernor.address)
-  );
-  await withConfirmation(
-    cAaveStrategyProxy.connect(sGuardian).claimGovernance()
-  );
-  log(`Claimed governance of AaveStrategy proxy with ${newGovernor.address}`);
+
+  if (isMainnet) {
+    // On Mainnet, only propose. The enqueue and execution are handled manually via multi-sig.
+    log("Sending transfer proposal to old governor...");
+    await sendProposal(propTransferArgs, propTransferDescription, {
+      governorAddr,
+    });
+    log("Transfer proposal sent.");
+
+    log("Sending claim proposal to new governor...");
+    await sendProposal(propClaimArgs, propClaimDescription);
+    log("Claim proposal sent.");
+  } else if (isFork) {
+    // On Fork we can send the proposal then impersonate the guardian to execute it.
+    log("Sending and executing transfer proposal...");
+    // Note: we send the proposal to the old governor by passing explicitly its address.
+    await executeProposal(propTransferArgs, propTransferDescription, {
+      governorAddr,
+    });
+
+    log("Sending and executing claim proposal...");
+    await executeProposal(propClaimArgs, propClaimDescription, {
+      guardianAddr: guardianAddr,
+    });
+  }
 };
 
 const baseName = path.basename(__filename);
@@ -70,6 +71,7 @@ const main = async () => {
 
 main.id = baseName;
 main.dependencies = [];
-main.tags = ["newGovernor"];
+main.tags = ["claimGovernance"];
+main.skip = () => isTest;
 
 module.exports = main;

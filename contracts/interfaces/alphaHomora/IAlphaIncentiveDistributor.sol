50a1b2bcf8ae9f50e18fff7431ae9e2bbffed80b
0xAnnapurna <96362165+0xAnnapurna@users.noreply.github.com>
2022-03-17 07:28:07 -0500

    fix slither

--- contracts/interfaces/alphaHomora/IAlphaIncentiveDistributor.sol
+++ contracts/interfaces/alphaHomora/IAlphaIncentiveDistributor.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
 interface IAlphaIncentiveDistributor {
     // closed source.
     // extracted from: https://snowtrace.io/address/0x7424ddc7ac9f60b3d0f7bca9e438dc2c1d44d043 (alpha)
